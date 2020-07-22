82cb18e8e7fc4f729c20c86eea01051df4a151b5
Rafael Queiroz <rafaelproinfo@gmail.com>
2020-07-22 02:02:37 -0300

    Add PSR4

--- src/Search.php
+++ src/Search.php
@@ -2,18 +2,34 @@
 
 namespace rafaelq80\digitalcep;
 
+use rafaelq80\digitalcep\ws\ViaCep;
+
 class Search
 {
-
-    private $url = "https://viacep.com.br/ws/";
 
     public function getAddressFromZipcode(string $zipCode): array
     {
 
         $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
+        return $this->getFromServer($zipCode);
+    }
 
-        return (array) json_decode($get);
+    private function getFromServer(string $zipCode): array
+    {
+
+        $get = new ViaCep();
+
+        return $get->get($zipCode);
+    }
+
+    private function processData($data)
+    {
+
+        foreach ($data as $k => $v) {
+            $data[$k] = trim($v);
+        }
+
+        return $data;
     }
 }
---
+++ src/ws/ViaCep.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace rafaelq80\digitalcep\ws;
+
+class ViaCep
+{
+
+    private $url = "https://viacep.com.br/ws/";
+
+    public function get(string $zipCode): array
+    {
+
+        $get = file_get_contents($this->url . $zipCode . "/json");
+
+        return (array) json_decode($get);
+    }
+}
